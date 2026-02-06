05bd00f01e4879ca40c66872ef2449f294fbffdd
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-06 23:06:58 +0100

    Create video control logic

--- 09-video/task/app.js
+++ 09-video/task/app.js
@@ -1,3 +1,32 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+
+
+const btns = document.querySelectorAll(".btn")
+const video = document.querySelector('.video')
+const payload = document.querySelector('.preloader__item')
+
+
+//DOMContentLoaded: This execute before the whole document gets loaded
+//Load: This execute once the whole document is loaded
+
+
+window.addEventListener("load", () => {
+    payload.style.display = "none"
+})
+
+
+
+const handleBtn = (e) => {
+    if (e.currentTarget.dataset.id === "play") {
+        video.play()
+    } else if (e.currentTarget.dataset.id === "pause") {
+        video.pause()
+    }
+}
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", handleBtn)
+})
