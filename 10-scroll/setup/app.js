60fc7c3a8893bb29fc95185574cb10a3bdad663a
Aduragbemi_Soladoye <segunsoladoye80@gmail.com>
2026-02-08 00:01:41 +0100

    Build the dynamic fixed state of the navbar and dynamic year rendering in the footer

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -11,3 +11,47 @@
 
 // ********** smooth scroll ************
 // select links
+
+
+const nav = document.querySelector('#nav')
+const date = document.querySelector('#date')
+
+
+//handle date
+const currentDate = new Date()
+const year = currentDate.getFullYear()
+
+date.textContent = year
+
+//nav dimension
+const navDim = nav.getBoundingClientRect()
+const navHeight = navDim.height
+
+console.log({navHe: navHeight})
+
+//state
+let scrollDistance = 0
+
+const setscrollDistance = (distance)=>{
+    //set state to distance
+    scrollDistance = distance
+    
+    //handle nav fixed state
+    navFixed(navHeight, scrollDistance)
+}
+
+const navFixed = (navH, scrollD)=>{
+    if(scrollD < navH){
+        nav.classList.remove('fixed-nav')
+    }else if(scrollD >= navH){
+        nav.classList.add('fixed-nav')
+    }
+}
+
+window.addEventListener("scroll", (e)=>{
+    //calculate a particular number of pixels moved from the top of the document
+    setscrollDistance(window.scrollY)
+
+    console.log({distance: scrollDistance})
+})
+
